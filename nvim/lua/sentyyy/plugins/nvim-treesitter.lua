8a2bf85bd6da262dc56d96358fc254d21854f47e
Kasyanov Maxim <maxkasser90@gmail.com>
2024-02-06 13:23:19 +0300

    Add nvim-treesitter

---
+++ nvim/lua/sentyyy/plugins/nvim-treesitter.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
+
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "lua",
+        "dockerfile",
+        "gitignore",
+        "bash",
+        "markdown",
+        "yaml",
+        "python",
+        "rust",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+},
+}
