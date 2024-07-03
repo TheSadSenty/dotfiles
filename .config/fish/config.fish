963fe152491633e811de68ec07da435ae62f4f4b
Kasyanov Maxim <maxkasser90@gmail.com>
2024-07-04 00:50:15 +0300

    Add pyenv and pyenv-virtualenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,17 @@
 if status is-interactive
-	# Commands to run in interactive sessions can go here
-	alias full_hd="xrandr --output Virtual1 --mode '1920x1080'"
-	alias cat="bat"
-	
-	set -gx EDITOR "nvim"
-	setxkbmap -layout us,ru -option 'grp:win_space_toggle'
-	set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
-	fish_add_path -a ~/.cargo/bin/
+    # Commands to run in interactive sessions can go here
+    alias full_hd="xrandr --output Virtual1 --mode '1920x1080'"
+    alias cat="bat"
+
+    set -gx EDITOR "nvim"
+    setxkbmap -layout us,ru -option 'grp:win_space_toggle'
+    set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
+    fish_add_path -a ~/.cargo/bin/
+
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+    pyenv init - | source
+    pyenv virtualenv-init - | source
+
 end
