0d1484ac66c7347be433dd4383e64d30c3142fd1
Kasyanov Maxim <maxkasser90@gmail.com>
2024-09-05 10:52:20 +0300

    Add SSH agent service
    Add auto start of SSH agent service
    Add auto start of Docker daemon

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias full_hd="xrandr --output Virtual1 --mode '1920x1080'"
-    alias cat="bat"
-
-    set -gx EDITOR "nvim"
-    setxkbmap -layout us,ru -option 'grp:win_space_toggle'
-    set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
-    fish_add_path -a ~/.cargo/bin/
-
-    set -Ux PYENV_ROOT $HOME/.pyenv
-    set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
-    pyenv init - | source
-    pyenv virtualenv-init - | source
-
 end
+
+xrandr --output Virtual1 --mode '1920x1080'
+
+alias cat="bat"
+
+set -gx EDITOR "nvim"
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+setxkbmap -layout us,ru -option 'grp:win_space_toggle'
+
+fish_add_path -a ~/.cargo/bin/
+
+pyenv init - | source
+pyenv virtualenv-init - | source
