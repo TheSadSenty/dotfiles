e5698567172442bdd2935f1f4ed59d5146740acd
Kasyanov Maxim <maxkasser90@gmail.com>
2024-10-20 16:13:42 +0300

    Add aliases for screen size

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias 1920x1080="xrandr --output Virtual1 --mode '1920x1080'"
+    alias 1366x768="xrandr --output Virtual1 --mode '1366x768'"
+
+    alias cat="bat"
 end
 
-xrandr --output Virtual1 --mode '1920x1080'
 
-alias cat="bat"
 
 set -gx EDITOR "nvim"
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
@@ -16,4 +18,3 @@ setxkbmap -layout us,ru -option 'grp:win_space_toggle'
 fish_add_path -a ~/.cargo/bin/
 
 pyenv init - | source
-pyenv virtualenv-init - | source
