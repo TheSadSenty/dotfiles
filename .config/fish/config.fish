3becd81184879c8b5d503fc7cb5d201741d783dc
Kasyanov Maxim <maxkasser90@gmail.com>
2024-12-03 21:16:15 +0300

    Remove pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,11 +10,8 @@ end
 
 set -gx EDITOR "nvim"
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
 setxkbmap -layout us,ru -option 'grp:win_space_toggle'
 
 fish_add_path -a ~/.cargo/bin/
 
-pyenv init - | source
