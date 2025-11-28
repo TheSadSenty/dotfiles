ff7f96ae33393db2f1fc4e0c8c4494700fe0990c
Kasyanov Maxim <maxkasser90@gmail.com>
2025-11-28 23:51:25 +0300

    (Neovim) Bring back blink.cmp instead of nvim-cmp

---
+++ .config/nvim/lua/sentyyy/plugins/blink.lua
@@ -0,0 +1,40 @@
+return {
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = 'default' },
+
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+    completion = {
+      documentation = { window = { border = 'single' }, auto_show = true },
+
+      ghost_text = { enabled = true },
+
+      menu = {
+        border = 'single',
+
+        -- nvim-cmp style menu
+        draw = {
+          columns = {
+            { 'label', 'label_description', gap = 1 },
+            { 'kind_icon', 'kind' },
+          },
+        },
+      },
+    },
+
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
+  },
+  opts_extend = { 'sources.default' },
+}
