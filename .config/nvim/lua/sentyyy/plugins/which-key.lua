0975c802c7c483dbe44ee1e5b88fdfaa8afd6e85
Kasyanov Maxim <maxkasser90@gmail.com>
2026-04-07 22:58:47 +0300

    (Neovim) Add Git group to which-key

--- .config/nvim/lua/sentyyy/plugins/which-key.lua
+++ .config/nvim/lua/sentyyy/plugins/which-key.lua
@@ -2,19 +2,15 @@ return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   config = function()
-    wk = require 'which-key'
+    local wk = require 'which-key'
 
     wk.add {
       { '<leader>c', group = '[C]ode' },
-      { '<leader>c_', hidden = true },
+      { '<leader>g', group = '[G]it' },
       { '<leader>d', group = '[D]ocument' },
-      { '<leader>d_', hidden = true },
       { '<leader>r', group = '[R]ename' },
-      { '<leader>r_', hidden = true },
       { '<leader>s', group = '[S]earch' },
-      { '<leader>s_', hidden = true },
       { '<leader>w', group = '[W]orkspace' },
-      { '<leader>w_', hidden = true },
     }
   end,
 }
