253c316f21926c1f6b189fefe3ff5e4cd57cf55d
Kasyanov Maxim <maxkasser90@gmail.com>
2026-04-11 22:01:05 +0300

    (Neovim) Add docs format picker in neogen

--- .config/nvim/lua/sentyyy/plugins/neogen.lua
+++ .config/nvim/lua/sentyyy/plugins/neogen.lua
@@ -1,10 +1,68 @@
 return {
   'danymat/neogen',
+  config = true,
+  keys = {
+    {
+      '<leader>dg',
+      function()
+        local neogen = require 'neogen'
+        local filetype = vim.bo.filetype
+
+        if filetype == '' or filetype == nil then
+          vim.notify('No filetype detected', vim.log.levels.WARN)
+          return
+        end
+
+        local formats = {
+          python = { 'google_docstrings', 'numpydoc', 'reST' },
+        }
+
+        local available_formats = formats[filetype]
+        if not available_formats then
+          neogen.generate()
+          return
+        end
+
+        local function generate_with_format(format)
+          neogen.generate {
+            annotation_convention = { [filetype] = format },
+          }
+          vim.notify('Generated docs with: ' .. format, vim.log.levels.INFO)
+        end
+
+        if #available_formats == 1 then
+          generate_with_format(available_formats[1].value)
+          return
+        end
+
+        vim.ui.select(available_formats, {
+          prompt = 'Select Neogen Format:',
+        }, function(choice)
+          if choice then
+            generate_with_format(choice)
+          end
+        end)
+      end,
+      desc = '[D]oc [G]enerate with format picker',
+    },
+    {
+      '<leader>dG',
+      function()
+        require('neogen').generate()
+      end,
+      desc = '[D]oc [G]enerate default',
+    },
+  },
   opts = {
     languages = {
       python = {
         template = {
-          annotation_convention = 'numpydoc',
+          annotation_convention = 'google_docstrings',
+        },
+      },
+      lua = {
+        template = {
+          annotation_convention = 'ldoc',
         },
       },
     },
