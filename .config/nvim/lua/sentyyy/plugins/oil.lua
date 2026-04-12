7a91ff79ea5398b4e46cbce43f4108ee90bc4ab0
Kasyanov Maxim <maxkasser90@gmail.com>
2026-04-12 12:52:45 +0300

    (Neovim) Updated oil.nvim config

--- .config/nvim/lua/sentyyy/plugins/oil.lua
+++ .config/nvim/lua/sentyyy/plugins/oil.lua
@@ -6,19 +6,27 @@ return {
       require('oil').setup {
         columns = { 'icon' },
         keymaps = {
-          ['<C-h>'] = false,
-          ['<M-h>'] = false,
+          ['cc'] = {
+            desc = 'Copy filepath to system clipboard',
+            callback = function()
+              require('oil.actions').copy_entry_path.callback()
+              vim.fn.setreg('+', vim.fn.getreg(vim.v.register))
+              vim.notify('Copied full path', vim.log.levels.INFO, { title = 'Oil' })
+            end,
+          },
         },
+        delete_to_trash = true,
         view_options = {
           show_hidden = true,
+          case_insensitive = true,
+        },
+        lsp_file_methods = {
+          autosave_changes = true,
         },
       }
 
       -- Open parent directory in current window
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory in current window' })
-
-      -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open parent directory in floating window' })
     end,
   },
 }
